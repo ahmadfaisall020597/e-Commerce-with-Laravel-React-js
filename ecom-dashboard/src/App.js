4b9152b5dca2e8f9961a1b42c24e236f76f1e8db
ahmadfaisal2513 <ahmadfaisal2513@gmail.com>
2022-05-11 12:04:28 +0700

    Installation ReactJs & Laravel Library

---
+++ ecom-dashboard/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-Commerce Dashboard</h1>
+      <button>Simple HTML Button</button>
+      <Button>Bootstrap Button</Button>
+      <Button>Bootstrap Button 2</Button>
+    </div>
+  );
+}
+
+export default App;
