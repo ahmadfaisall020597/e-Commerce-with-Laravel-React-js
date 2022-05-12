db996290dcde160881c4bad6fe744401584e4bcd
ahmadfaisal2513 <ahmadfaisal2513@gmail.com>
2022-05-12 07:23:42 +0700

    Revisi Tampilan & Route Dashboard ReactJs

--- ecom-dashboard/src/App.js
+++ ecom-dashboard/src/App.js
@@ -2,24 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import {Button} from 'react-bootstrap';
 import Header from './Header';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import AddProduct from './AddProduct';
 import UpdateProduct from './UpdateProduct';
-function Home(){
-  return (
-    <div>
-    <Header />
-    <h1>E-Commerce Dashboard</h1>
-    </div>
-  );
-}
+// function Home(){
+//   return (
+//     <div>
+//     <Header />
+//     <h1>E-Commerce Dashboard</h1>
+//     </div>
+//   );
+// }
 function App() {
   return (
     <div className="App">
+      <Header />
+      <h1>E-Commerce Dashboard</h1>
       <Routes>
-      <Route path="/" element={<Home />}></Route>
+      {/* <Route path="/" element={<Home />}></Route> */}
       <Route path="Login" element={<Login />}></Route>
       <Route path="Register" element={<Register />}></Route>
       <Route path="Add" element={<AddProduct />}></Route>
--- ecom-dashboard/src/Register.js
+++ ecom-dashboard/src/Register.js
@@ -1,21 +1,6 @@
-import {Navbar} from "react-bootstrap";
-import {Container} from "react-bootstrap";
-import {Nav} from "react-bootstrap";
-import {Link} from "react-router-dom";
 function Register(){
     return(
         <div>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">e-Commerce</Navbar.Brand>
-                    <Nav className="me-auto navbar_wrapper">
-                        <Link to="/login">Login</Link>
-                        <Link to="/register">Register</Link>
-                        <Link to="/Add">Add Product</Link>
-                        <Link to="/update">Update Product</Link>
-                    </Nav>
-                </Container>
-            </Navbar>
             <h1>Halaman Register</h1>
         </div>
     )
