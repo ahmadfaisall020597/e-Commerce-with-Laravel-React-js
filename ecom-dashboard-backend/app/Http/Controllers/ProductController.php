c36036aa12796c3d716c7a7d9621996d5a296743
ahmadfaisal2513 <ahmadfaisal2513@gmail.com>
2022-05-15 07:55:04 +0700

    Error Database API, cek di controller sma models laravel

---
+++ ecom-dashboard-backend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Handler\Proxy;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    function addProduct(Request $req)
+    {
+        // return $req->file('file')->store('products'); // untuk membuat atau upload file gambar dengan API. location Storage/App/Product
+        $product = new Product;
+        $product->name = $req->input('name');
+        // $product->file_path = $req->file('file')->store('products');
+        $product->description = $req->input('description');
+        $product->price = $req->input('price');
+        $product->save();
+        return $req->input();
+    }
+}
